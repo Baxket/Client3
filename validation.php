aab5bfabf25d8cd12fea439cc7ef71d8debb0891
Baxket <64781098+Baxket@users.noreply.github.com>
2020-10-26 23:35:58 +0000

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,23 +1,5 @@
 <?php
-/*
- $con=mysqli_connect('localhost','root','');
- mysqli_select_db($con,'userregistration');
- $name=$_POST['user'];
- $pass=$_POST['password'];
 
- $s="select * from usertable where name='$name' && password='$pass'";
- $result=mysqli_query($con,$s);
- $num=mysqli_num_rows($result);
- if($num==1){
- 	$_SESSION['username']=$name;
- 	header('location:Userdetails.php');
- }
- else{
- 	header('location:Userlogin.php');
- 	
- }
-
-?>*/
 
  if((isset($_POST['user'])) && !(empty($_POST['user']))&&(isset($_POST['password'])) && !(empty($_POST['password'])))
  {
@@ -42,9 +24,8 @@ fwrite($file, "1" );
 
 
 $re = file_get_contents($fn);
-//echo $str;
-  //echo "string  $first_line";
-$first_line = "Succesful Validation of Client Device";
+
+
  if($first_line == "Succesful Validation of Client Device" || $first_line == "Validation failed"){
  	 session_start();
              $_SESSION['username'] = $_POST['user'];
