bc61efe6c2a7fe2525a5b2db15466e94c90ffbb4
Baxket <64781098+Baxket@users.noreply.github.com>
2020-10-26 23:37:24 +0000

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,52 +1,15 @@
 <?php
 
 
- if((isset($_POST['user'])) && !(empty($_POST['user']))&&(isset($_POST['password'])) && !(empty($_POST['password'])))
- {
 
- $name=$_POST['user'];
- $pass=$_POST['password'];
 
  $old_path=getcwd();
  chdir('CS');
-$output=shell_exec("sh initial.sh $name $pass");
+$output=shell_exec("sh initial.sh c97026 335138");
 
  chdir($old_path);
  echo "$output";
- $first_line = strtok($output,".");
- echo "$first_line";
-
- $fn = "status.php"; 
-$file = fopen($fn, "w+"); 
-$size = filesize($fn); 
-$text = fread($file, 2);
-fwrite($file, "1" );
-
-
-$re = file_get_contents($fn);
-
-
- if($first_line == "Succesful Validation of Client Device" || $first_line == "Validation failed"){
- 	 session_start();
-             $_SESSION['username'] = $_POST['user'];
-              $_SESSION['password'] = $_POST['password'];
-              $_SESSION['output']= $output;
-             
-
-              header('location:Userdetails.php');
-              session_write_close();
- 	
-
- 	
- 	
- }
- else{echo "  ";
-
-
-
-}
-
-
+ 
 }
 
 ?>
