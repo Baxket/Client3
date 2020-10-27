175079d5ebeba5770fb75383f128fa77333073b7
Baxket <64781098+Baxket@users.noreply.github.com>
2020-10-27 21:28:31 +0000

    Update Userdetails.php

--- Userdetails.php
+++ Userdetails.php
@@ -40,7 +40,7 @@ $r = file_get_contents($a);
     <title>Client Page</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="dp.png">
     <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
